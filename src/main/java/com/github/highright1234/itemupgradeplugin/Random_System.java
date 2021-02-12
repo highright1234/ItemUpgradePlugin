58228571899c734b3666e6abf4528546da065c7c
Sleepmin <tickiock08@gmail.com>
2021-02-12 09:51:22 +0900

    update

--- src/main/java/com/github/highright1234/itemupgradeplugin/Random_System.java
+++ src/main/java/com/github/highright1234/itemupgradeplugin/Random_System.java
@@ -1,5 +1,7 @@
 package com.github.highright1234.itemupgradeplugin;
 
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -8,8 +10,13 @@ import org.bukkit.event.inventory.InventoryType;
 public class Random_System implements Listener {
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event) {
-        if (event.getInventory().getType() != InventoryType.SMITHING) {
-            // TODO
+        if (event.getClickedInventory().getType() != InventoryType.SMITHING) {
+            if (event.getCursor().getItemMeta().hasCustomModelData()) {
+                if (event.getCurrentItem().getItemMeta().getCustomModelData() == 2) {
+                    event.getCurrentItem().setType(Material.AIR);
+                    // TODO random
+                }
+            }
         }
     }
 }
